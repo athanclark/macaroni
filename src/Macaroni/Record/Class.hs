5767aefdc7ba67c508b4b2b26ead4756af854867
Athan Clark <athan.clark@gmail.com>
2018-07-13 13:41:54 -0600

    using abbreviated CRUD operations

--- src/Macaroni/Record/Class.hs
+++ src/Macaroni/Record/Class.hs
@@ -14,8 +14,7 @@ import Data.Text (Text)
 import Data.Aeson (ToJSON, FromJSON)
 import Database.Persist.Sql (SqlBackend)
 import Database.Persist.Class (PersistEntity)
-import Database.KingFisher.Class
-  (GetStorable, SetStorable, GetAllStorable, NewStorable, DeleteStorable)
+import Database.KingFisher.Class (CRUDStorable, BasicCRUDStorable, GetStorable, GetAllStorable)
 -- NOTE Macaroni will store the _key_ of the data view, which represents _total_
 -- content of the symbol
 import Control.Monad.Reader (ReaderT)
@@ -46,16 +45,33 @@ instance EncodeRecord a a where
 
 
 class
-  ( GetStorable key getRecord symbol (ReaderT SqlBackend IO)
-  , GetStorable key (Synopsis symbol) symbol (ReaderT SqlBackend IO)
-  , GetAllStorable () (Synopsis symbol) symbol (ReaderT SqlBackend IO)
-  , NewStorable key newRecord symbol (ReaderT SqlBackend IO)
-  , SetStorable key (SetError symbol) setRecord symbol (ReaderT SqlBackend IO)
-  , DeleteStorable key (DeleteError symbol) symbol (ReaderT SqlBackend IO)
+  ( BasicCRUDStorable key (BasicSetError symbol) (BasicDeleteError symbol)
+      getRecord setRecord newRecord symbol (ReaderT SqlBackend IO)
+  , GetStorable key (BasicSynopsis symbol) symbol (ReaderT SqlBackend IO)
+  , GetAllStorable () (BasicSynopsis symbol) symbol (ReaderT SqlBackend IO)
   ) => MacaroniBasicRecord key getRecord setRecord newRecord symbol
        | getRecord -> key symbol
        , setRecord -> key symbol
        , newRecord -> key symbol where
-  type Synopsis symbol :: *
-  type SetError symbol :: *
-  type DeleteError symbol :: *
+  type BasicSynopsis symbol :: *
+  type BasicSetError symbol :: *
+  type BasicDeleteError symbol :: *
+
+
+
+class
+  ( CRUDStorable
+    (parentKey, key) getRecord
+    key newRecord
+    (parentKey, key) (ParentedSetError symbol) setRecord
+    (parentKey, key) (ParentedDeleteError symbol)
+    symbol (ReaderT SqlBackend IO)
+  , GetStorable (parentKey, key) (ParentedSynopsis symbol) symbol (ReaderT SqlBackend IO)
+  , GetAllStorable parentKey (ParentedSynopsis symbol) symbol (ReaderT SqlBackend IO)
+  ) => MacaroniParentedRecord parentKey key getRecord setRecord newRecord symbol
+       | getRecord -> key symbol
+       , setRecord -> key symbol
+       , newRecord -> key symbol where
+  type ParentedSynopsis symbol :: *
+  type ParentedSetError symbol :: *
+  type ParentedDeleteError symbol :: *
